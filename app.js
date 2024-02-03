2daf0872d171e21fa582747f71a83129659ecd17
root <root@based.lol>
2024-02-03 07:33:34 +0000

    This and that

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
+function swap() {
+    if (document.querySelector('.code').classList.contains('hidden')) {
+        document.querySelector('button').innerText = 'Run'
+        document.querySelector('button').onclick = run;
+    } else {
+        document.querySelector('button').innerText = 'Back to code'
+        document.querySelector('button').onclick = swap;
+    }
+
+    document.querySelector('.output-container').classList.toggle('hidden');
+    document.querySelector('.code').classList.toggle('hidden');
+    document.querySelector('span.label').classList.toggle('stdout');
+}
+
 async function run() {
     let code = document.querySelector('.code').value;
     let input = document.querySelector('.input').value;
@@ -9,18 +23,41 @@ async function run() {
     data.append('input', input);
 
     let r = await fetch('/run.ty', {
-            method: 'POST',
-            body: data
+        method: 'POST',
+        body: data
     });
 
     document.querySelector('.output').innerText = await r.text();
+
+    swap();
+}
+
+async function share() {
+    let code = document.querySelector('.code').value;
+    let input = document.querySelector('.input').value;
+
+    let data = new FormData();
+    data.append('code', code);
+    data.append('input', input);
+
+    let r = await fetch('/share.ty', {
+        method: 'POST',
+        body: data
+    });
+
+    alert(await r.text());
 }
 
 async function load(name) {
-    let r = await fetch(name + '.ty');
+    let r = await fetch(`/${name}.ty`);
     let example = await r.json();
+
     document.querySelector('.code').value = example.code;
     document.querySelector('.input').value = example.input || '';
+
+    if (document.querySelector('.code').classList.contains('hidden')) {
+        swap();
+    }
 }
 
 const examples = document.querySelectorAll('.example');
@@ -33,4 +70,8 @@ for (const e of examples) {
     });
 }
 
-examples[0].click();
+if (window.location.search === '') {
+    examples[0].click();
+} else {
+    load(window.location.search.slice(1))
+}
